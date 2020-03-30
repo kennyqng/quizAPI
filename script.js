f22144a3c306541bd5526809cbc487ec00cfecab
hadooken <kennyqng@gmail.com>
2020-03-30 01:26:06 -0700

    looping

--- script.js
+++ script.js
@@ -7,11 +7,20 @@
 // array of answers
 // local storage
 // 
+var messageTop = document.getElementById("messageTop");
+var messageBottom = document.getElementById("messageBottom");
+var info = document.getElementById("infoBox");
+var buttonsBox = document.getElementById("response");
+
 var time = 60;
 var timeDisplay = document.getElementById("timeDisplay");
+var userSelection = "";
+var points = 0;
 var questionsArray =[
     {question:"Commonly used data types DO NOT include:",
-    choices:["Strings","Boolean","Alerts", "Numbers"]}, 
+    choices:["Strings","Boolean","Alerts", "Numbers"],
+    correctAnswer:"Alerts"},
+
     {question:"The condition in an if/else statement is enclosed within _______.",
     choices:["Quotes", "Curly brackets", "Parenthesis", "Square brackets"]}, 
     {question: "Arrays in JavaScript can be used to store ________.",
@@ -23,15 +32,6 @@ var questionsArray =[
 
     // questionsArray[0].question; 
     // questionsArray[0].choices[0];
-
-var messageTop = document.getElementById("messageTop");
-var info = document.getElementById("infoBox");
-var buttonsBox = document.getElementById("response");
-
-console.log(messageTop.textContent);
-
-messageTop.textContent = questionsArray[2];
-console.log(messageTop.textContent);
 
 
 // start page
@@ -47,30 +47,57 @@ console.log(messageTop.textContent);
 messageTop.textContent = "Coding Quiz Challenge";
 info.textContent = "Try to Answer the following code-related questions within the time limit. Keep in mind that incorrect response will penalize your time by ten seconds.";
 var start = document.createElement("button");
-buttonsBox.appendChild(start);
+messageBottom.appendChild(start);
 start.textContent = "Start";
 start.addEventListener("click", function(){
+    event.preventDefault();
     start.remove();
     info.textContent = "";
+    console.log("arry length: "+questionsArray.length);
     questionGenerator(0);
 })
 
 
 
 function questionGenerator(number){
+
     messageTop.textContent = questionsArray[number].question;
     for (var i = 0; i < questionsArray[number].choices.length; i++){
         var btn = document.createElement("button");
         btn.setAttribute("id", "answer"+i);
+        btn.setAttribute("class", "btn-answer");
         btn.textContent = questionsArray[number].choices[i];
         buttonsBox.appendChild(btn);
     }
+    
+    var answerBtn = document.getElementsByClassName("btn-answer");
+    for (var i = 0; i < answerBtn.length; i++){
+        answerBtn[i].addEventListener("click", 
+        function (){
+            userSelection = this.textContent;
+            console.log("correct answer " + questionsArray[number].correctAnswer);
+            console.log("user selected " + userSelection);
+            console.log("compare "+userSelection.localeCompare(questionsArray[number].correctAnswer));
+            if(userSelection == questionsArray[number].correctAnswer){
+                points++;
+                console.log("points: " + points);
+            }
+        });
+    }
+    
+
+
+    // $(document).on("click", ".btn-answer", function(){
+    //     userSelection = this.textContent;
+    //     console.log(userSelection);
+    // })
+
 }
+
+
 function questionRemover(number){
     for (var i = 0; i < questionsArray[number].choices.length; i++){
         var a1 = document.getElementById("answer"+i);
         a1.remove();
     }
-    
-
 }
